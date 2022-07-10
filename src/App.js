d6098bc73389b3a142722616eebe65ed9a654b4a
Alwin <meijboom.am@gmail.com>
2022-07-10 22:28:24 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="hero is-info is-fullheight">
+        <div className="hero-body">
+          <div className="">
+            <p className="title">
+              MythicalSora.dev
+            </p>
+            <p className="subtitle">
+              Under Construction
+            </p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
